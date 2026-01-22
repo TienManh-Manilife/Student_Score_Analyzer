820f70683fbac864970a2cce715ad0f9a11e94fd
Nguyen Tien Manh <manilife217@gmail.com>
2026-01-22 22:45:15 +0700

    Hàm tính toán khoảng dự đoán thời gian, điểm số

--- libs/ability/ability_graduation_lib.py
+++ libs/ability/ability_graduation_lib.py
@@ -1,5 +1,20 @@
 from libs.database_lib.database import *
 from libs.database_lib.evaluate_student_lib import *
+from libs.regression.regression_lib import get_R2, get_a_b_of_linear_regression
 
-def get_time_to_write(mssv):
-    return get_mean_time_all_HocKy(mssv)
+def get_percent(mssv):
+    return 1 - get_R2(mssv)
+
+def get_period_of_time_to_write(mssv):
+    time = get_mean_time_all_HocKy(mssv)
+    return round(time*(1-get_percent(mssv)), 2), round(time*(1+get_percent(mssv)), 2)
+
+def predict_of_score_through_linear_regression(mssv, time):
+    a, b = get_a_b_of_linear_regression(mssv)
+    return round(a*time + b, 2)
+
+def predict_period_of_score_to_write(mssv):
+    low_time, high_time = get_period_of_time_to_write(mssv)
+    low_score = predict_of_score_through_linear_regression(mssv, low_time)
+    high_score = predict_of_score_through_linear_regression(mssv, high_time)
+    return low_score, high_score
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from libs.database_lib.database import *
 from libs.database_lib.evaluate_student_lib import *
+from libs.ability.ability_graduation_lib import *
 from tui.app.analyzer import AnalyzerApp
 
 if __name__ == "__main__":
     # AnalyzerApp().run()
-    print(get_mean_time_all_HocKy(24020220))
+    for mssv in get_all_MSSV():
+        a, b = predict_period_of_score_to_write(mssv)
+        print(a, b)
